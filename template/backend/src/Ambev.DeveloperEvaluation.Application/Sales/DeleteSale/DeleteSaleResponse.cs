4ff6d0ff9352e0bc18774340df384a592c050bd4
Daniel Almeida <dansteam.c6@gmail.com>
2024-11-24 23:15:18 -0400

    feat: Adds deleteSale structure.

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale;
+
+public class DeleteSaleResponse
+{
+    public bool Success { get; set; }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/DeleteSale/DeleteSaleProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.DeleteSale;
+
+public class DeleteSaleProfile : Profile
+{
+    public DeleteSaleProfile()
+    {
+        //CreateMap<Guid, DeleteSaleCommand>()
+        //    .ConstructUsing(id => new DeleteSaleCommand { Id = id });
+    }
+}
